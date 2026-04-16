a0cd9f21e808c52cd3c8a53debc67f417b588596
Brandon <fornesrubio@gmail.com>
2026-04-16 07:25:50 -0700

    Catalogo Tipos Herramientas

--- Server/Controllers/HerramientaController.cs
+++ Server/Controllers/HerramientaController.cs
@@ -31,5 +31,36 @@ namespace AdminHerramientas.Server.Controllers
             return Ok();
         }
 
+        [HttpPut("{id}")]
+        public async Task<IActionResult> EditarTipoHerramienta(int id,Herramienta herramienta)
+        {
+            if (id != herramienta.Id) return BadRequest("El ID de la herramienta no coincide.");
+
+            int filasAfectadas = await _context.Herramientas
+               .Where(h => h.Id == herramienta.Id)
+               .ExecuteUpdateAsync(s => s
+                   .SetProperty(h => h.Tipo, herramienta.Tipo)
+               );
+            if (filasAfectadas == 0) return NotFound();
+
+            return Ok();
+
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTipoHerramienta(int id)
+        {
+            var herramienta = await _context.Herramientas.FindAsync(id);
+            if (herramienta == null)
+            {
+                return NotFound("La herramienta no fue encontrada.");
+            }
+            herramienta.Activo = false;
+
+            _context.Entry(herramienta).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
     }
 }
